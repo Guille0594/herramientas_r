c21e782d414ce111350933c5e6d38ec90c2049ce
guillermo <guillermogd94@gmail.com>
2021-10-18 11:04:38 +0200

    Añadimos el modulo 1 introducctorio

---
+++ 1_modulo_intro/fichero_clase_1.r
@@ -0,0 +1,88 @@
+1+1
+
+2+4
+3+7
+alturas <-c(1.64, 1.81, 2.01, 1.75)
+mean (alturas)
+class(alturas)
+23.6
+length(alturas)
+mean(alturas)
+median(alturas)
+var(alturas)
+sd(alturas)
+min(alturas)
+max(alturas
+    )
+#La moda lo veremos luego más adelante
+alturas[2
+        ]
+alturas[c(1, 3
+          )]
+my_data_frame <- data.frame(
+  col1= 1:4,
+  col2=c("category1","category2","category3"),
+  col3= c(3.4,-2.5,10.1,-0.05,
+  col4=c(T,F,F,T)
+))
+  
+airquality
+
+iris 
+heairis$Sepal.Lenght
+iris$Sepal.Lenght>6.5
+iris
+iris
+
+#Calculo la media de la anchura del petalo
+#Compruebo que filas tienen anchura del sepalo mayor que la media
+#Extraigo la longitud del sepalo de esas flores
+
+
+media_sepalo <- mean(iris$Sepal.Width)
+media_sepalo
+flores_con_anchura_mayor<- iris$Sepal.With > media_sepalo
+flores_con_anchura_mayor
+iris$Sepal.Lenght[flores_con_anchura_mayor]
+
+longitudes<-iris$Sepal.Length[iris$Sepal.Width > mean(iris$Sepal.With)]
+mean(longitudes)
+
+table()
+iris$Species
+
+table(iris$Species)
+iris$
+mean(pi)
+mean(suma)
+iris
+media_sepalo
+media_sepalo <- iris$mean(iris.Sepal.Width)
+media_sepalo<- mean(iris$Sepal.Width)
+media_sepalo
+
+#Vamos a calcular las medias de cada columna
+#Media de la longitud del sepalo
+
+media_longitud_sepalo <- mean(iris$Sepal.Lenght)
+iris
+media_lomgitud_sepalo <- mean(iris$Sepal.Lenght)
+2+2
+x<-2**4
+media_longitud_sepalo<-mean(iris$Sepal.Length)
+media_longitud_sepalo
+
+#Media anchura sepalo
+
+media_anchura_sepalo<- mean(iris$Sepal.Width)
+media_anchura_sepalo
+
+#Media longitud petalo
+
+media_longitud_petalo<-mean(iris$Petal.Length)
+media_longitud_petalo
+
+#Media anchura petalo
+
+media_anchura_petalo<- mean(iris$Petal.Width)
+media_anchura_petalo
